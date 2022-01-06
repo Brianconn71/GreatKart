e57569e351e6c7b70c7808e3adbe72df158a7911
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-06 18:14:11 +0000

    add: added custom user and set up superuser functionality

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,37 @@ class MyAccountManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
+        
+        if not username:
+            raise ValueError('User must have a username')
+        
+        user = self.model(
+            email = self.normalize_email(email), # normalize_email makes it lowercase essentially
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+    
 
 
 class Account(AbstractBaseUser):
@@ -14,7 +45,7 @@ class Account(AbstractBaseUser):
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.Charfield(max_length=50)
+    phone_number = models.CharField(max_length=50)
     
     # Required fields
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -27,10 +58,12 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     
+    objects = MyAccountManager()
+    
     def __str__(self):
         return self.email
     
-    def has_perm(self, perm, obj-None):
+    def has_perm(self, perm, obj=None):
         return self.is_admin
     
     def has_module_perms(self, add_label):
