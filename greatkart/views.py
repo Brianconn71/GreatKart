46b726a4425de76dd250dcd62aff1210df0c6a70
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-10 18:02:18 +0000

    set up homme page django loop and store paage set up

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
+    products = Product.objects.all().filter(is_available=True)
+    
+    context = {
+        'products': products,
+    }
     template = 'home.html'
-    return render(request, template)
+    return render(request, template, context)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    context = {}
+    template= "store/store.html"
+    return render(request, template, context)
