e70748e0d9ed30f30af2a53d6ea45af8960048fd
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-11 17:57:29 +0000

    added store page set up

--- store/views.py
+++ store/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+    
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
     template= "store/store.html"
     return render(request, template, context)
