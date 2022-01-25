a9c94badc2799a4a73f19653775c65c958d5fee2
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-25 17:58:40 +0000

    added cart model and working on session id

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def _cart_id(request):
+    
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.ogjects.get(cart_id=)
+
+
 def cart(request):
     template = 'store/cart.html'
     return render(request, template)
