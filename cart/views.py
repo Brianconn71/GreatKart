59feab35116e739bc4d9fafa5739cdf0f3621801
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-31 18:05:49 +0000

    add: added cart functionality to cart template

--- cart/views.py
+++ cart/views.py
@@ -34,11 +34,24 @@ def add_cart(request, product_id):
             cart = cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
     return redirect('cart')
 
 
-def cart(request):
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectNotExist:
+        pass # ignore it
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        
+    }
+    
     template = 'store/cart.html'
-    return render(request, template)
+    return render(request, template, context)
