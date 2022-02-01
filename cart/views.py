f7164f977efc666daed9ed531448067e908581a5
Brian Connolly <Brian.connolly71@gmail.com>
2022-02-01 17:53:29 +0000

    add: added tax and gran total to cart page and setting up decrement btn

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from store.models import Product
 from .models import Cart, CartItem
@@ -37,6 +37,18 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -44,12 +56,16 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = round((2 * total)/100,2)
+        grand_total = round(tax + total, 2)
     except ObjectNotExist:
         pass # ignore it
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
         
     }
     
