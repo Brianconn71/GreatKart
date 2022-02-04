44ebcc9c7798662d3087c8ffb21a0ee026dd8374
Brian Connolly <Brian.connolly71@gmail.com>
2022-02-04 16:35:11 +0000

    fixed add to cart links

--- store/views.py
+++ store/views.py
@@ -26,6 +26,7 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request))
     except Exception as e:
         raise e
     
