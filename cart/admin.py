6413bad2e3bc5819f21fbef9a9be15a979bb6b76
Brian Connolly <Brian.connolly71@gmail.com>
2022-03-22 18:03:53 +0000

    added variations to cart model

--- cart/admin.py
+++ cart/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Cart, CartItem
 
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    list_display = 
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
